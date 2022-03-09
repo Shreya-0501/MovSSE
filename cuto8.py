0b774f2876fdb985c259da752e39cb633f7e411a
rrCk <86021238+rrCk@users.noreply.github.com>
2022-03-09 10:46:53 +0530

    Update cuto8.py
    Added NER and made json files according to our requirements.

--- cuto8.py
+++ cuto8.py
@@ -2,84 +2,73 @@ import string
 import re
 import pprint
 import stanza
-#import spacy
-#from spacy import displacy
 from collections import Counter
-#import en_core_web_sm
-#nlp = en_core_web_sm.load()
-#spacy.load('en_core_web_trf')
+import json
 
-#THE BREAKFAST CLUB
 
-#class Segregate :
+file = open("bttf.txt", encoding="utf-8")                                   #script file
+fileSub = open("bttfSc.txt", encoding="utf-8")                               #subtitle file
 
-#file = open("tbcsc.txt", "r")                                   #script file
-#fileSub = open("tbcsub.txt", "r")                               #subtitle file
 
-#file = open("bttf.txt", "r")                                   #script file
-#fileSub = open("bttfSc.txt", "r")                               #subtitle file
-
-file = open("hangsc.txt", "r")                                   #script file
-fileSub = open("hangsub.txt", "r")                               #subtitle file
-
-# Reading from file
 Content = file.read()                                           #full script file
 contentSub = fileSub.read()                                     #full subtitle file
 
-CoList2 = Content.split("CUT TO:")                              #stores cut to - cut to
+Scriptpart = Content.split("CUT TO:")                              #stores cut to - cut to
 listSub = contentSub.split("\n\n")                              #splits the subtitle file paragraph to  paragraph (dialogue no., timestamp, dialogue)
 
+
 listSubLower = []                                               #has the content in lowercase
-CoList2Low = []
+ScriptpartLow = []
 
 for i in listSub:                                               #converting everything in listSub to lowercase and appending it to a new list
     x = i.lower()
     listSubLower.append(x)
 
-for i in CoList2:                                               #converting everything in CoList2 to lowercase and appending it to a new list
+for i in Scriptpart:                                               #converting everything in CoList2 to lowercase and appending it to a new list
     x = i.lower()
-    CoList2Low.append(x)
+    ScriptpartLow.append(x)
 
 
-CoListFullf = []
+Scriptedit1 = []
 
-for i in CoList2Low:                                            #making short forms of have to full forms
+for i in ScriptpartLow:                                            #making short forms of have to full forms
     x = i.replace("'ve", " have")
     x = x.replace("...", " ")
     x = x.replace("--", " ")
-    CoListFullf.append(x)
+    x = x.replace("\n\n"," ")
+    x = x.replace("\n"," ")
 
-#print(CoListFullf)
+    Scriptedit1.append(x)
 
-listSubFullf = []
+
+Subedit1 = []
 
 for i in listSubLower:                                          #making short forms of have to full forms
     x = i.replace("'ve", " have")
-    listSubFullf.append(x)
+    Subedit1.append(x)
 
 punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''                       #punctuation
 
-CoList3 = []                                                    #has lowercase content without '\n' chars
-CoListn = []
+Scriptedit2 = []                                                    #has lowercase content without '\n' chars
 
-for i in CoListFullf:                                           #removing all the '\n' chars and appending it to a new list
-    #x = i
-    x = i.replace("\n\n", " ")
-    x = x.replace("\n", " ")
-    #x = x.replace("  ", " ")
-    for ele in x:
-        if ele in punc:
-            x = x.replace(ele, "")
-    CoList3.append(x)
 
-#print(CoList3)
+for i in Scriptedit1:                                           #removing all the '\n' chars and appending it to a new list
+    
+    x = i
 
-dict = {0: {"dialogueNo" : "0", "cut to" : 0, "timeStamp" : "00:00:00", "dialogue" : " ", "change" : False}}
+    for ele in punc:
+        x = x.replace(ele, "")
+
+    Scriptedit2.append(x)
+
+#print(Scriptedit2)
+
+dict = {0: {"dialogueNo" : "0", "scene" : 0, "timeStamp" : "00:00:00", "dialogue" : " ", "change" : False}}
 
 def dictMaker(s, idx, subDialogue):                      #makes a dictionary
     dict[subDialogue[0]] = {}
     dict[subDialogue[0]]["dialogueNo"] = subDialogue[0]
-    dict[subDialogue[0]]["cut to"] = idx
+    dict[subDialogue[0]]["scene"] = idx
     dict[subDialogue[0]]["timestamp"] = subDialogue[1]
 
     str1 = ""                                           #empty string
@@ -90,18 +79,13 @@ def dictMaker(s, idx, subDialogue):                      #makes a dictionary
     dict[subDialogue[0]]["dialogue"] = str1             #stores dialogue(string)
     dict[subDialogue[0]]["change"] = False              #stores where the cut-to num changes
 
-#for cut in CoList3:
-    #print(cut , "\n")
-
-#print(*CoList2, sep = "\n\n")
-#print(*listSub, sep = "\n\n")
 
 before = 0
 flag = 0
-counter = 0                                       #stores the number of dialogues that aren't found
-count = 0                                         #stores the number of dialogues found in script file from the subtitle file
+notfoundc = 0                                       #stores the number of dialogues that aren't found
+foundc = 0                                         #stores the number of dialogues found in script file from the subtitle file
 
-for j in listSubFullf:                            #has paragraphs of subtitles   --> we are splitting every paragraph into:
+for j in Subedit1:                            #has paragraphs of subtitles   --> we are splitting every paragraph into:
     subDialogue = j.split("\n")                   #{"1486", "02:49:52,390 --> 02:49:55,225'", "I'm not gonna be coming back,to this place."}
 
     if len(subDialogue) < 2:                      # then dialogue doesn't exist
@@ -114,72 +98,174 @@ for j in listSubFullf:                            #has paragraphs of subtitles  
         if ele in punc:
             x = x.replace(ele, "")
 
-    for idx, i in enumerate(CoList3):                            #idx is iterator(cut to number), i has script in cut to - cut to (in paragraphs)
-        if ((idx<=before+3) and (idx>before-1)):                 #to ensure the dialogue is in the present index or in the ones after this
+    for idx, i in enumerate(Scriptedit2):                            #idx is iterator(scene number), i has cuto no in cut to - cut to (in paragraphs)
+        if ((idx<=before+2) and (idx>before-1)):                 #to ensure the dialogue is in the present index or in the ones after this
             if(i.find(x) != -1):                                 #if we find the dialogue
                 #print(subDialogue[0],x , idx)                   #print the dialogue no., dialogue and the cut-to index
                 s = slice(2, len(subDialogue))                   #contains full dialogue
-                dictMaker(s, idx, subDialogue)                   #passing sliced dialogue, cut to index, and subDialogue list
+                dictMaker(s, idx, subDialogue)                   #passing sliced dialogue, scene index, and subDialogue list
                 flag = 1
-                count +=1
-                #print(count)                                    #prints the number of dialogues found
+                foundc +=1
+                #print(foundc)                                    #prints the number of dialogues found
                 before = idx
                 break
 
             else:
-                if flag == 0 :
+                if (flag == 0) :
                     fcount = 0                                         #found count
                     sentence = x.split(" ")                            #dialogue -> word by word  , x-->dialogue
                     totcount = len(sentence)                           #size of the dialogue
                     for word in sentence:                              #for every word in dialogue
-                        if (CoList3[before].find(word) != -1):         #searching word in CoList3()
+                        if (Scriptedit2[before].find(word) != -1):         #searching word in Scriptedit2()
                             fcount += 1
 
                     percentage = fcount/totcount
 
-                    if(percentage >= 0.75):
-                        #print(subDialogue[0], "   ",x , before, "hoi found it by the new else")
+                    if(percentage >= 0.65):
                         #print(subDialogue[0], x , before)
                         s = slice(2, len(subDialogue))                          #slices the dialogue from the subDialogue list
                         dictMaker(s, idx, subDialogue)
-                        count += 1
+                        foundc += 1
                         flag = 1
+                        before = idx
+                        break
+                    else:
+                        # print(subDialogue[0],x , before,percentage)
+                        s = slice(2, len(subDialogue))                          #slices the dialogue from the subDialogue list
+                        dictMaker(s, before, subDialogue)
 
+        
     if flag == 0:                                               #not found dialogues
-        counter += 1
-        #print(counter)                                         #prints the number of dialogues not found
+        notfoundc += 1
+        #print(subDialogue[0],x , idx)
+        #print(notfoundc)                                         #prints the number of dialogues not found
 
 cutonum = 0
 
 for dictNum, info in dict.items():                              #dictNum -> high level key  #initial index number of dictionary -->initial high level key
     for key in info:                                            #info -> the inner dict's key  (dialogueNo, change)
-        if cutonum != info["cut to"]:                           #if the number changes
-            dict[dictNum]["change"] = True                      #True --> when cut to num changes
-    cutonum = info["cut to"]                                    #initially initialised to the cut to num
+        if cutonum != info["scene"]:                           #if the number changes
+            dict[dictNum]["change"] = True                      #True --> when scene num changes
+    cutonum = info["scene"]                                    #initially initialised to the scene num
 
-print("Number of dialogues not found :-", counter)
-print("Number of dialogues found :-", count)
+print("Number of dialogues found :-", foundc)
+print("Number of dialogues not found :-", notfoundc)
+
 
 #pprint.pprint(dict, sort_dicts = False)
 
-#nlp = stanza.Pipeline('en')                    #for stanza
-nlp = stanza.Pipeline(lang='en', processors='tokenize,ner')
+# for id,data in dict.items():
 
-#print(CoList2[5])
-#character[]
+#     #print(dict[x]['change'])
 
-for para in CoList2:
+#     for key in data:
+#         if(key=="change"):
+#             if(data[key] == True):
+#                 print(id ,data["scene"], data[key])
+
+
+# for id, data in dict.items():
+#     for key in data:
+#         if(key == "timestamp"):
+#             #print(data[key])
+#             times = data[key].split("-->")
+#             print(times[0])
+
+
+final = {0: {"dialogueNo" : "0", "scene" : 0, "start" : "00:00:00" , "end" : "00:00:00", "dialogue" : " "}}
+
+for id, data in dict.items():
+    if id ==0 or id == 1:
+        continue
+    final[id] ={}
+    for key in data:
+
+        if(key == "timestamp"):
+            times = data[key].split(" --> ")
+            
+            final[id]["start"] = times[0]
+            final[id]["end"] = times[1]
+        elif(key == "change"):
+            continue
+        else:
+            final[id][key] = data[key]
+
+pprint.pprint(final, sort_dicts = False)
+
+
+script = {0: {"scene no" : 0, "data" :" " , "GPE" :[] ,"PERSON":[],"WOA":[],"ORG":[],"TIME":[],"MONEY":[]}}
+
+
+
+nlp = stanza.Pipeline('en')                    #for stanza
+
+
+for idx, para in enumerate(Scriptedit1):
     doc = nlp(para)
+
+    script[idx]={}
+    script[idx]["scene no"] = idx
+    script[idx]["data"] = para
+
+    geo = []
+    persons = []
+    workoa = []
+    org = []
+    time = []
+    money = []
+
     for ent in doc.ents:
-        if ent.type == 'PERSON':
-            print("Person: ", ent.text)
-        if ent.type == 'GPE':
-            print("GPE:    ",ent.text)
+        
+        #print(ent.type, ent.text)
 
-    print("\n###\n")
-#doc2 = nlp("hawaii.")                          #for stanza
-#print(doc2)
+        if ent.type == "GPE":
+            #print(ent.text)
+            if ent.text not in geo:
+                geo.append(ent.text)
+                #print(ent.text)
+        elif ent.type == "PERSON":
+            #print(ent.text)
+            if ent.text not in persons:
+                persons.append(ent.text)
+                #print(ent.text)
+        elif ent.type == "WORK OF ART":
+            #print(ent.text)
+            if ent.text not in workoa:
+                workoa.append(ent.text)
+                #print(ent.text)
+        elif ent.type == "ORG":
+            #print(ent.text)
+            if ent.text not in org:
+                org.append(ent.text)
+                #print(ent.text)
+        elif ent.type == "TIME":
+            #print(ent.text)
+            if ent.text not in time:
+                time.append(ent.text)
+                #print(ent.text)
+        elif ent.type == "MONEY":
+            #print(ent.text)
+            if ent.text not in money:
+                money.append(ent.text)
+                #print(ent.text)
+        
+    script[idx]["GPE"]= geo
+    script[idx]["PERSON"] = persons
+    script[idx]["WOA"] = workoa
+    script[idx]["ORG"]= org
+    script[idx]["TIME"] = time
+    script[idx]["MONEY"] = money
+    #print(geo,persons,workoa,org,time,money)
 
-# doc = nlp(CoList3[5])
-#
-# pprint([(X.text, X.label_) for X in doc.ents])
+
+pprint.pprint(script, sort_dicts = False)
+
+with open("bfftdailogues.json", "w") as outfile:
+    json.dump(final, outfile)
+
+with open("bfftscene.json","w") as outfile:
+    json.dump(script,outfile)
+
+
+
+
